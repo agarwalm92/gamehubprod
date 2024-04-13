03c36825d6fc1b328138093e1c6f2f73fdb95b76
Mayank <agarwalm92@gmail.com>
2024-04-13 22:12:40 +0530

    Adding skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,20 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGetGames from "../hooks/useGetGames";
 import GameCard from "./GameCard";
+import GameGridSkeleton from "./GameGridSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGetGames();
+  const { games, error, loading } = useGetGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 3, lg: 3, xl: 5 }}
         spacing={10}
         padding={10}
       >
+        {loading && skeletons.map((i) => <GameGridSkeleton key={i} />)}
         {games.map((i) => (
           <GameCard key={i.id} game={i} />
         ))}
--- src/hooks/useGetGames.ts
+++ src/hooks/useGetGames.ts
@@ -22,14 +22,20 @@ interface FetchGames {
 const useGetGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     ApiClient.get<FetchGames>("/games")
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((e) => {
         setError(e.message);
+        setLoading(false);
       });
   }, []);
-  return { games, error };
+  return { games, error, loading };
 };
 export default useGetGames;
