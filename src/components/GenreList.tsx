f34d9196b7b7b87a163072befebb9ba8f743d18a
Mayank <agarwalm92@gmail.com>
2024-04-15 19:55:01 +0530

    Highlighting selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
   onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectedGenre }: Props) => {
+const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const { data, error, loading } = useGetGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   if (error) return null;
@@ -17,12 +18,18 @@ const GenreList = ({ onSelectedGenre }: Props) => {
     <List>
       {loading && skeletons.map((i) => <GenreListSkeleton key={i} />)}
       {data.map((g) => (
-        <ListItem key={g.id} paddingY="5px">
+        <ListItem
+          key={g.id}
+          paddingY="5px"
+          backgroundColor={g.id == selectedGenre?.id ? "green" : ""}
+          borderRadius="10px"
+        >
           <HStack>
             <Image
               src={CroppedImageUrl(g.image_background)}
               boxSize="32px"
               borderRadius={8}
+              marginX="5px"
             />
             <Button
               onClick={() => onSelectedGenre(g)}
