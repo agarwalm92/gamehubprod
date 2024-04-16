20e89243749b994c682c096ac0389fe536850d47
Mayank <agarwalm92@gmail.com>
2024-04-16 23:47:10 +0530

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import useGetGenres, { Genre } from "../hooks/useGetGenres";
 import CroppedImageUrl from "../services/CroppedImageUrl";
 import GenreListSkeleton from "./GenreListSkeleton";
@@ -15,33 +22,41 @@ const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   // if (loading) return <Spinner />; //for spinner use this and include it in the imports, below is skeleton example
 
   return (
-    <List>
-      {loading && skeletons.map((i) => <GenreListSkeleton key={i} />)}
-      {data.map((g) => (
-        <ListItem
-          key={g.id}
-          paddingY="5px"
-          backgroundColor={g.id == selectedGenre?.id ? "green" : ""}
-          borderRadius="10px"
-        >
-          <HStack>
-            <Image
-              src={CroppedImageUrl(g.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-              marginX="5px"
-            />
-            <Button
-              onClick={() => onSelectedGenre(g)}
-              variant="link"
-              fontSize="lg"
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {loading && skeletons.map((i) => <GenreListSkeleton key={i} />)}
+        {data.map((g) => (
+          <ListItem
+            key={g.id}
+            paddingY="5px"
+            backgroundColor={g.id == selectedGenre?.id ? "green" : ""}
+            borderRadius="10px"
+          >
+            <HStack>
+              <Image
+                src={CroppedImageUrl(g.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                marginX="5px"
+                objectFit="cover"
+              />
+              <Button
+                onClick={() => onSelectedGenre(g)}
+                variant="link"
+                fontSize="lg"
+                whiteSpace="normal"
+                textAlign="left"
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
